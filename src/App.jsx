e6fb6dcd7b4fc2cc05e784523cba8f3b833692de
이재석 <127112461+jaeseok14@users.noreply.github.com>
2025-06-30 20:25:03 +0900

    커밋

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,105 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 
 function App() {
-  const [num, setNum] = useState(0)
+  // const [num, setNum] = useState(0)
 
   return (
     <>
-      <p>{num}</p>
-      <button onClick={() => setNum(num + 1)}>+1</button>
+      {/* <p>{num}</p>
+      <button onClick={() => setNum(num + 1)}>+1</button> */}
+      <table border={1}>
+          <tr>
+            <td>재민</td>
+            <td>재민</td>
+            <td>재민</td>
+            <td>재민</td>
+            <td>재민</td>
+          </tr>
+          <tr>
+            <td>재민</td>
+            <td>재민</td>
+            <td>재민</td>
+            <td>재민</td>
+            <td>재민</td>
+          </tr>
+          <tr>
+            <td>재민</td>
+            <td>재민</td>
+            <td>재민</td>
+            <td>재민</td>
+            <td>재민</td>
+          </tr>
+          <tr>
+            <td>재민</td>
+            <td>재민</td>
+            <td>재민</td>
+            <td>재민</td>
+            <td>재민</td>
+          </tr>
+          <tr>
+            <td>재민</td>
+            <td>재민</td>
+            <td>재민</td>
+            <td>재민</td>
+            <td>재민</td>
+          </tr>
+          <tr>
+            <td>재민</td>
+            <td>재민</td>
+            <td>재민</td>
+            <td>재민</td>
+            <td>재민</td>
+          </tr>
+          <tr>
+            <td>재민</td>
+            <td>재민</td>
+            <td>재민</td>
+            <td>재민</td>
+            <td>재민</td>
+          </tr>
+          <tr>
+            <td>재민</td>
+            <td>재민</td>
+            <td>재민</td>
+            <td>재민</td>
+            <td>재민</td>
+          </tr>
+          <tr>
+            <td>재민</td>
+            <td>재민</td>
+            <td>재민</td>
+            <td>재민</td>
+            <td>재민</td>
+          </tr>
+          <tr>
+            <td>재민</td>
+            <td>재민</td>
+            <td>재민</td>
+            <td>재민</td>
+            <td>재민</td>
+          </tr>
+          <tr>
+            <td>재민</td>
+            <td>재민</td>
+            <td>재민</td>
+            <td>재민</td>
+            <td>재민</td>
+          </tr>
+          <tr>
+            <td>재민</td>
+            <td>재민</td>
+            <td>재민</td>
+            <td>재민</td>
+            <td>재민</td>
+          </tr>
+          <tr>
+            <td>재민</td>
+            <td>재민</td>
+            <td>재민</td>
+            <td>재민</td>
+            <td>재민</td>
+          </tr>
+      </table>
     </>
   )
 }
